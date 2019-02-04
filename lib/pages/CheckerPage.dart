683f8313bf1026fda7d4a327c4600859c1ed5afd
pegorenkov <epipasha@gmail.com>
2019-02-04 16:00:43 +0300

    Add keyboard action search

--- lib/pages/CheckerPage.dart
+++ lib/pages/CheckerPage.dart
@@ -16,6 +16,12 @@ class CheckerPageState extends State<CheckerPage> {
   final TextEditingController controller =
       TextEditingController(text: '5500009866193');
 
+  void _searchByText(){
+    if (_formKey.currentState.validate()) {
+      Navigator.pushNamed(context, '/flowers/${controller.text}');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return ScopedModelDescendant<MainModel>(
@@ -25,52 +31,52 @@ class CheckerPageState extends State<CheckerPage> {
           title: Text(AppLocalizations.of(context).priceChecker),
         ),
         body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Image.asset('assets/logo7fl.jpg'),
-                Form(
-                  key: _formKey,
-                  child: Container(
-                    padding: EdgeInsets.symmetric(horizontal: 16.0),
-                    child: Row(
-                      children: <Widget>[
-                        Flexible(
-                          child: TextFormField(
-                            controller: controller,
-                            decoration: InputDecoration(
-                                labelText: AppLocalizations.of(context)
-                                    .barcodeSearchFieldTitle),
-                            maxLength: 13,
-                            keyboardType: TextInputType.number,
-                            validator: (String value) {
-                              if (value.length < 13) {
-                                return AppLocalizations.of(context)
-                                    .errorBarcodeLength;
-                              }
-                            },
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Image.asset('assets/logo7fl.jpg'),
+                  Form(
+                    key: _formKey,
+                    child: Container(
+                      padding: EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Row(
+                        children: <Widget>[
+                          Flexible(
+                            child: TextFormField(
+                              textInputAction: TextInputAction.search,
+                              onFieldSubmitted: (_){_searchByText();},
+                              controller: controller,
+                              decoration: InputDecoration(
+                                  labelText: AppLocalizations.of(context)
+                                      .barcodeSearchFieldTitle),
+                              maxLength: 13,
+                              keyboardType: TextInputType.number,
+                              validator: (String value) {
+                                if (value.length < 13) {
+                                  return AppLocalizations.of(context)
+                                      .errorBarcodeLength;
+                                }
+                              },
+                            ),
                           ),
-                        ),
-                        SizedBox(
-                          width: 16.0,
-                        ),
-                        RaisedButton(
-                          child: Text(
-                            AppLocalizations.of(context).search.toUpperCase(),
-                            style: Theme.of(context).textTheme.button,
+                          SizedBox(
+                            width: 16.0,
                           ),
-                          color: Theme.of(context).accentColor,
-                          onPressed: () {
-                            if (_formKey.currentState.validate()) {
-                              Navigator.pushNamed(context, '/flowers/${controller.text}');
-                            }
-                          },
-                        ),
-                      ],
+                          RaisedButton(
+                            child: Text(
+                              AppLocalizations.of(context).search.toUpperCase(),
+                              style: Theme.of(context).textTheme.button,
+                            ),
+                            color: Theme.of(context).accentColor,
+                            onPressed: () {_searchByText();},
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
         ),
         floatingActionButton: FloatingActionButton(
