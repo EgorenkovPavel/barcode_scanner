c9d0a2d07a0d67a066dcdf5b4a07a7a97cae8ac1
pegorenkov <epipasha@gmail.com>
2019-02-05 16:10:12 +0300

    Add search history

--- lib/pages/CheckerPage.dart
+++ lib/pages/CheckerPage.dart
@@ -4,6 +4,8 @@ import 'package:scoped_model/scoped_model.dart';
 import '../Localization.dart';
 import '../MainModel.dart';
 
+import '../widgets/FlowerList.dart';
+
 class CheckerPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -16,7 +18,7 @@ class CheckerPageState extends State<CheckerPage> {
   final TextEditingController controller =
       TextEditingController(text: '5500009866193');
 
-  void _searchByText(){
+  void _searchByText() {
     if (_formKey.currentState.validate()) {
       Navigator.pushNamed(context, '/flowers/${controller.text}');
     }
@@ -24,71 +26,74 @@ class CheckerPageState extends State<CheckerPage> {
 
   @override
   Widget build(BuildContext context) {
+    final bool showFab = MediaQuery.of(context).viewInsets.bottom == 0.0;
     return ScopedModelDescendant<MainModel>(
         builder: (BuildContext context, Widget child, MainModel model) {
       return Scaffold(
         appBar: AppBar(
           title: Text(AppLocalizations.of(context).priceChecker),
         ),
-        body: Center(
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Image.asset('assets/logo7fl.jpg'),
-                  Form(
-                    key: _formKey,
-                    child: Container(
-                      padding: EdgeInsets.symmetric(horizontal: 16.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Container(
-                            width: 150.0,
-                            child: TextFormField(
-                              textInputAction: TextInputAction.search,
-                              onFieldSubmitted: (_){_searchByText();},
-                              controller: controller,
-                              decoration: InputDecoration(
-                                  labelText: AppLocalizations.of(context)
-                                      .barcodeSearchFieldTitle, ),
-                              maxLength: 13,
-                              keyboardType: TextInputType.number,
-                              validator: (String value) {
-                                if (value.length < 13) {
-                                  return AppLocalizations.of(context)
-                                      .errorBarcodeLength;
-                                }
-                              },
-                            ),
-                          ),
-                          SizedBox(
-                            width: 16.0,
-                          ),
-                          RaisedButton(
-                            child: Text(
-                              AppLocalizations.of(context).search.toUpperCase(),
-                              style: Theme.of(context).textTheme.button,
-                            ),
-                            color: Theme.of(context).accentColor,
-                            onPressed: () {_searchByText();},
-                          ),
-                        ],
+        body: Column(
+          children: <Widget>[
+            Image.asset('assets/logo7fl.jpg'),
+            Form(
+              key: _formKey,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Flexible(
+                      child: TextFormField(
+                        textInputAction: TextInputAction.search,
+                        onFieldSubmitted: (_) {
+                          _searchByText();
+                        },
+                        controller: controller,
+                        decoration: InputDecoration(
+                          labelText: AppLocalizations.of(context)
+                              .barcodeSearchFieldTitle,
+                        ),
+                        maxLength: 13,
+                        keyboardType: TextInputType.number,
+                        validator: (String value) {
+                          if (value.length < 13) {
+                            return AppLocalizations.of(context)
+                                .errorBarcodeLength;
+                          }
+                        },
                       ),
                     ),
-                  ),
-                ],
+                    SizedBox(
+                      width: 16.0,
+                    ),
+                    RaisedButton(
+                      child: Text(
+                        AppLocalizations.of(context).search.toUpperCase(),
+                        style: Theme.of(context).textTheme.button,
+                      ),
+                      color: Theme.of(context).accentColor,
+                      onPressed: () {
+                        _searchByText();
+                      },
+                    ),
+                  ],
+                ),
               ),
             ),
+            Expanded(child: Container(child: FlowerList())),
+          ],
         ),
-        floatingActionButton: FloatingActionButton(
-            child: Image.asset("assets/barcode-scan.png"),
-            onPressed: () {
-              model.scanBarcodeOnCamera().then((String barcode) {
-                if (barcode.isEmpty) return;
-                Navigator.pushNamed(context, '/flowers/$barcode');
-              });
-            }),
+        floatingActionButton: showFab
+            ? FloatingActionButton(
+                child: Image.asset("assets/barcode-scan.png"),
+                onPressed: () {
+                  model.scanBarcodeOnCamera().then((String barcode) {
+                    if (barcode.isEmpty) return;
+                    Navigator.pushNamed(context, '/flowers/$barcode');
+                  });
+                })
+            : null,
       );
     });
   }
--- lib/widgets/FlowerList.dart
+++ lib/widgets/FlowerList.dart
@@ -3,21 +3,27 @@ import 'package:scoped_model/scoped_model.dart';
 
 import '../MainModel.dart';
 import '../objects/flower.dart';
+import '../ConnectionSettings.dart';
 
-class FlowerList extends StatelessWidget{
+class FlowerList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScopedModelDescendant<MainModel>(
-      builder: (BuildContext context, Widget child, MainModel model){
+      builder: (BuildContext context, Widget child, MainModel model) {
         return ListView.builder(
+          shrinkWrap: true,
           itemCount: model.flowerList.length,
-          itemBuilder: (BuildContext context, int position){
-            Flower flower = model.flowerList[position];
+          itemBuilder: (BuildContext context, int position) {
+            Flower _flower = model.flowerList[position];
 
             return ListTile(
-              leading: CircleAvatar(child: Image.asset(flower.photoPath),),
-              title: Text(flower.title),
-              trailing: Text('$flower.regularPrice'), //replace with price widget
+              leading: CircleAvatar(
+                child: Image.network(_flower.photoPath,
+                    headers: ConnectionSettings.headers),
+              ),
+              title: Text(_flower.title),
+              trailing:
+                  Text('${_flower.regularPrice} ₽'), //replace with price widget
             );
           },
         );
